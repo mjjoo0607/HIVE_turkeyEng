3b7374f0266f619ca2862afc103f86f5825f0e1d
mjjoo0607 <minjoojoo67@gmail.com>
2017-08-07 17:04:31 -0500

    Update main.js

--- Project1/main.js
+++ Project1/main.js
@@ -25,7 +25,7 @@ $(function() {
 
 //     settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_78y5vvwkxJZrLtH';
 	 // settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_0rFAtUZh2zg7oA5';
-	    settings.defaultredirect = 'https://iastate.qualtrics.com/SE/?SID=SV_eDMbyZaMaeG0ynb';
+	    settings.defaultredirect = 'https://iastate.qualtrics.com/jfe/form/SV_29mHjQrYk2zRFf7';
 	
 	// **Tasklength**     
     // Length of the group introduction task in milliseconds. Can be changed to any number (in ms). Default: 180000 (3min) 
